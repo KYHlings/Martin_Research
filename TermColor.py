438fe010dbcace4b03038f53570c71229aecaba9
Martin <martin.drugge@telia.com>
2020-10-13 09:56:21 +0200

    -

---
+++ TermColor.py
@@ -0,0 +1,16 @@
+from termcolor import colored, cprint
+cprint(f'Hello World', 'red')
+cprint(f'Hello World', 'red', 'on_blue')
+print(colored("Hello", 'red'), colored("World", 'blue'))
+
+print(f'{colored("sknsdflsnf2", "green")} sadsadasdlal')
+
+cprint('    #########', 'red')
+cprint('   ###########', 'red')
+cprint('  #############', 'red')
+cprint(' ###############', 'red')
+cprint(' ###############', 'white')
+cprint('  #############', 'white')
+cprint('   ###########', 'white')
+cprint('    #########', 'white')
+
